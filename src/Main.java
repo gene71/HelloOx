9b8c7e0976e5cfbdbe3a064e616478bf4cb70ae3
gene <gene@main>
2018-05-09 13:58:34 -0600

    test poll scm

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Jenkins Pipeline Test");
-		System.out.println("build 18?");
+		System.out.println("build 21?");
 
 	}
 
